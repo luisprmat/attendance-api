06e06e18e1618a2cacb71a9d9eb80906a5169256
Luis Antonio Parrado <luisprmat@gmail.com>
2021-09-20 12:34:09 -0500

    endpoints to students

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -38,7 +38,8 @@ class RouteServiceProvider extends ServiceProvider
         $this->configureRateLimiting();
 
         $this->routes(function () {
-            Route::prefix('api')
+            Route::prefix('api/v1')
+                ->name('api.v1.')
                 ->middleware('api')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/api.php'));
